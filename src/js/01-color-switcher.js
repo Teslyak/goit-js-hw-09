4323fdaa68f187bd656870a5b76336be7d4efd9e
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-07-07 11:08:02 +0300

    hw9-task1-end

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    body: document.querySelector('body'),
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]')
+}
 
+console.log(refs.body);
+let timerId = null;
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick)
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.start.setAttribute('disabled', 'disabled') ;
+};
+
+function onStopClick() {
+    clearInterval(timerId);
+    refs.start.removeAttribute('disabled');
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
