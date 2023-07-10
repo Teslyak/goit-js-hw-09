e9558184b312dc75338733b3c9cdd0d8becf96ed
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-07-10 10:36:34 +0300

    hw09-task3-up

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -11,7 +11,7 @@ refs.form.addEventListener('submit', onSubmitBtn);
 
 function onSubmitBtn(e) {
   e.preventDefault();
-  for (i = 1; i <= refs.amount.value; i += 1) {
+  for ( let i = 1; i <= refs.amount.value; i += 1) {
     if (i === 1) {
       delay = Number(refs.delay.value);
       createPromise(i, delay);
